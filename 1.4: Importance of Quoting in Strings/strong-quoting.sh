f072474778a3f681ca8a7ec91e1f62d98d6d70f8
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2018-09-06 21:11:36 +0300

    added some scripts

---
+++ 1.4: Importance of Quoting in Strings/strong-quoting.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+world="World"
+echo 'Hello $world' #using strong quoting will print the text as it is
---
+++ 1.4: Importance of Quoting in Strings/weak-quoting.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+world="World"
+echo "Hello $world" #weak quoting will replace $world with its value
